04130b4b84cc92872efbcd023761a2c2395ab836
BallinGuy <tballin2000@hotmail.com>
2022-11-10 00:57:13 -0500

    addRole completed

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ function menu(){
         });
 }
 function viewEmployee(){
-    db.query(`SELECT id AS ID, first_name AS 'First Name', last_name AS 'Last Name', role.title as Title, department.name AS Department, role.salary AS Salary FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id`, function (err, results) {
+    db.query(`SELECT employee.id AS ID, employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title as Title, department.name AS Department, role.salary AS Salary FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id`, function (err, results) {
         console.log('\n');
         console.table(results);
         menu();
@@ -123,7 +123,7 @@ function addEmployee(){
                 db.query(`SELECT id FROM employee WHERE CONCAT(first_name, ' ', last_name) = ?`, response.manager, function (err, results) {
                     managerId = results[0].id;
                     db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`, [response.first_name, response.last_name, roleId, managerId], function (err, results) {
-                        console.log('\n New employee successfully added');
+                        console.log('\n New employee successfully added\n');
                         menu();
                     });
                 });
@@ -140,18 +140,49 @@ function updateEmployee(){
     menu();
 }
 function viewRole(){
-    db.query(`Select title AS Title, department.name AS Department, salary AS Salary FROM role JOIN department ON role.department_id = department.id`, function (err, results) {
+    db.query(`Select role.title AS Title, department.name AS Department, role.salary AS Salary FROM role JOIN department ON role.department_id = department.id`, function (err, results) {
         console.log('\n');
             console.table(results);
             menu();
     });
 }
 function addRole(){
-    console.log("Role Added");
-    menu();
+    var departmentArray = [];
+    db.query('SELECT name FROM department', (err, results) => {
+        for (i = 0; i < results.length; i++) {
+            departmentArray.push(results[i].name);
+        }});
+    inquirer.prompt ([
+        {
+            type: 'input',
+            message: 'What is the name of the new role?',
+            name: 'role'
+        },
+        {
+            type: 'number',
+            message: 'What is the salary of the new role?',
+            name: 'salary'
+        },
+        {
+            type: 'list',
+            message: `Choose the department this role works in`,
+            choices: departmentArray,
+            name: 'department'
+        },
+    ]).then(response => {
+        var departmentId;
+            db.query(`SELECT id FROM department WHERE name = ?`, response.department, function (err, results) {
+                departmentId = results[0].id;
+                db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);`, [response.role, response.salary, departmentId], function (err, results) {
+                    console.log('\n New role successfully added \n');
+                    menu();
+                });
+            });
+            
+    });
 }
 function viewDepartment(){
-    db.query(`Select name FROM department`, function (err, results) {
+    db.query(`Select name AS Department FROM department`, function (err, results) {
         console.log('\n');
             console.table(results);
             menu();
@@ -162,11 +193,11 @@ function addDepartment(){
     inquirer.prompt ([
         {
             type: 'input',
-            message: 'What is the name of the department?',
+            message: 'What is the name of the new department?',
             name: 'department'
         }
-    ]).then(answers => {
-        db.query(`INSERT INTO department (name) VALUES (?);`, answers.department, function (err, results) {
+    ]).then(response => {
+        db.query(`INSERT INTO department (name) VALUES (?);`, response.department, function (err, results) {
                 console.log(`New Department successfully created`);
                 menu();
         });
